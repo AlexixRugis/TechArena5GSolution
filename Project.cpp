beda36e15a82f991f4d4638926002a160617784e
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2024-04-30 20:00:50 +0300

    executing tests from file

---
+++ Project.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Solution.h"
+
+using namespace std;
+
+#define MAX_TESTS 10
+
+int main()
+{
+  ifstream ifs("open.txt");
+  
+  int t;
+  ifs >> t;
+
+#ifdef MAX_TESTS
+  t = min(t, MAX_TESTS);
+#endif // MAX_TESTS
+
+  int testNumber = 1;
+  while (t--) {
+    int n, m, k, j, l;
+    ifs >> n >> m >> k >> j >> l;
+
+    vector<Interval> reserved(k);
+    for (size_t i = 0; i < k; i++) {
+      int start, end;
+      ifs >> start >> end;
+      reserved[i].start = start;
+      reserved[i].end = end;
+    }
+
+    vector<UserInfo> users(n);
+    for (size_t i = 0; i < n; i++) {
+      int rbNeed, beam;
+      ifs >> rbNeed >> beam;
+      users[i].id = i;
+      users[i].rbNeed = rbNeed;
+      users[i].beam = beam;
+    }
+
+    vector<Interval> output = Solver(n, m, k, j, l, reserved, users);
+
+    cout << "Test: " << testNumber++ << endl;
+    cout << "Intervals: " << output.size() << endl;
+    cout << left;
+    cout << setw(10) << "Begin" << setw(10) << "End" << setw(10) << "Users" << endl;
+    for (const auto& interval : output) {
+      cout << setw(10) << interval.end << setw(10) << interval.end;
+      for (const auto& user : interval.users) {
+        cout << user << ' ';
+      }
+      cout << endl;
+    }
+  }
+  
+}
---
+++ Solution.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <vector>
+
+struct Interval {
+  int start, end;
+  std::vector<int> users;
+};
+
+struct UserInfo {
+  int rbNeed;
+  int beam;
+  int id;
+};
+
+/// <summary>
+/// Функция решения задачи
+/// </summary>
+/// <param name="N">Количество пользователей</param>
+/// <param name="M">Количество блоков передачи данных</param>
+/// <param name="K">Количество зарезервированных интервалов передачи данных</param>
+/// <param name="J">Максимальное количество интервалов с пользователями</param>
+/// <param name="L">Максимальное количество пользователей на одном интервале</param>
+/// <param name="reservedRBs">Зарезервированные интервалы</param>
+/// <param name="userInfos">Информация о пользователях</param>
+/// <returns>Интервалы передачи данных, до J штук</returns>
+std::vector<Interval> Solver(int N, int M, int K, int J, int L,
+  std::vector<Interval> reservedRBs,
+  std::vector<UserInfo> userInfos) {
+  std::vector<Interval> answer;
+  //your algorithm 
+  answer.push_back({
+    1, 10,
+    {0, 1 , 2, 3}
+    });
+  return answer;
+}
